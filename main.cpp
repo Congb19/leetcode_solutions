9add9267a25bfc9cf95df464e552f18d08522b88
Congb19 <347006087@qq.com>
2019-09-29 23:42:50 +0800

    9.29

--- main.cpp
+++ main.cpp
@@ -8,39 +8,34 @@
 #define ture true
 
 using namespace std;
-int maxArea(vector<int>& height) {
-    int res;
-    int t=0;
-    int max=0;
-    //思路1 暴力循环乘法找最大值。n?
-//    for (int i = 0; i < height.size()-1; ++i) {
-//        for (int j = i+1; j < height.size(); ++j) {
-//            t=min(height[i], height[j])*(j-i);
-//            max=t>max?t:max;
-//        }
-//    }
-//    res=max;
-    //思路2 优化一下，从两侧开始移动，避开面积铁定会减少的方式（算是动态规划）
-    int q=0, e=height.size()-1;
-    while (q<e) {
-        t=min(height[q], height[e])*(e-q);
-        max=t>max?t:max;
-        if (height[q]>height[e]) e--;
-        else q++;
+string intToRoman(int num) {
+    string res="";
+    vector<string> t;
+    char a[7]={'I','V','X','L','C','D','M'};
+    // 思路1 递归返回叠加 感觉不是很好
+    if (num>=1000) {
+        for (int i = 0; i < 4; ++i) {
+
+        }
     }
-    res=max;
+    else if(num>=100) {
+
+    }
+    else if (num>10) {
+
+    }
+    else {
+
+    }
+    //思路2 直接除法，看末尾，分情况加数字即可
+
     return res;
 }
 
 int main() {
     string s="ab";
-    string p=".*";
-
-    string s1="aabbbbbbb";
-    string s2="aab**.*";
-    int k=123321;
-
+    int k=3998;
     vector<int> h={1,8,6,2,5,4,8,3,7};
-    cout << maxArea(h) << endl;
+    cout << intToRoman(k) << endl;
     return 0;
 }
