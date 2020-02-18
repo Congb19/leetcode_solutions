0dc7306d524f0e0685d68c427f4c03ba5adb640d
Congb19 <347006087@qq.com>
2020-02-18 20:51:05 +0800

    2.18 34 268

---
+++ js/268.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    if (nums.length===1) {
+        if (nums[0]===1) return 0;
+        else return 1;
+    }
+    nums.sort((a,b) => {return a-b});
+    // console.log(nums);
+    for (let i=0; i<nums.length; i++) {
+        if(i!=nums[i]) return i;
+    }
+    return nums.length;
+};
+let nums=[3,0,1];
+console.log(missingNumber(nums));
