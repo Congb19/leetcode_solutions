34c0d309f2fc3898433dbbf82acf9d6aa925d679
Congb19 <347006087@qq.com>
2020-07-28 18:01:53 +0800

    7.28

---
+++ js/j34.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function (root, sum) {
+	let res = [...Array()];
+	if (root == null) return [...Array()];
+	let dd = (node, t, s) => {
+		if (node !== null) {
+			t.push(node.val);
+			if (node.left == null && node.right == null) {
+				if (s + node.val == sum) res.push([...t]);
+			} else {
+				dd(node.left, t, s + node.val);
+				dd(node.right, t, s + node.val);
+			}
+			t.pop();
+		}
+	};
+	dd(root, [...Array()], 0);
+	return res;
+};
