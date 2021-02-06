d2c76db6b0979244878678efb08823289c322a17
Congb19 <347006087@qq.com>
2021-02-06 17:07:40 +0800

    2.6 1423

--- test2.js
+++ test2.js
@@ -1,16 +1,22 @@
-while ((line = readline())) {
-	var lines = line.split(" ");
-	var a = parseInt(lines[0]);
-	let arr = [...Array(a + 1)];
-	arr[0] = 1;
-	arr[1] = 1;
-	if (a < 2) {
-		print(1);
-	} else {
-		for (let i = 2; i <= a; i++) {
-			arr[i] = arr[i - 1] + arr[i - 2];
-		}
-		print(arr[a]);
-	}
-}
+// while ((line = readline())) {
+// 	var lines = line.split(" ");
+// 	var a = parseInt(lines[0]);
+// 	let arr = [...Array(a + 1)];
+// 	arr[0] = 1;
+// 	arr[1] = 1;
+// 	if (a < 2) {
+// 		print(1);
+// 	} else {
+// 		for (let i = 2; i <= a; i++) {
+// 			arr[i] = arr[i - 1] + arr[i - 2];
+// 		}
+// 		print(arr[a]);
+// 	}
+// }
 // https://exam.nowcoder.com/cts/17067885/summary?id=9A8DACD9DA155A9A
+
+const fs = require("fs");
+var data = fs.readFileSync("input.txt");
+console.log("同步读取: " + data.toString());
+
+console.log("程序执行完毕。");
