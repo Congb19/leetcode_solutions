497d8c1c94f3c5e5cdbb2e2a1bd1cf0fa18188a9
congb19 <347006087@qq.com>
2021-03-31 17:53:58 +0800

    3.30 74

---
+++ js/74.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+  let r = matrix.length;
+  let c = matrix[0].length;
+  let i = r - 1, j = 0;
+  if (matrix[i][j] == target) return true;
+  while (i >= 0 && matrix[i][j] > target) {
+    if (matrix[i][j] == target) return true;
+    i--;
+  }
+  if (i < 0) return false;
+  if (matrix[i][j] == target) return true;
+  // else return true;
+  while (j < c && matrix[i][j] < target) {
+    if (matrix[i][j] == target) return true;
+    j++;
+    if (matrix[i][j] == target) return true;
+    if (matrix[i][j] > target) return false;
+  }
+  return false;
+};
+console.log(searchMatrix([[1], [3]], 1));
