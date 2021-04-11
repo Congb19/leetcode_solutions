17fdf2f0f36a015a575b8f170f3e4176c7a33875
congb19 <347006087@qq.com>
2021-04-11 19:00:38 +0800

    4.11 264

---
+++ js/264.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var nthUglyNumber = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  let p2 = 1, p3 = 1, p5 = 1;
+  for (let i = 2; i <= n; i++) {
+    const num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+    dp[i] = Math.min(Math.min(num2, num3), num5);
+    if (dp[i] === num2) p2++;
+    if (dp[i] === num3) p3++;
+    if (dp[i] === num5) p5++;
+  }
+  return dp[n];
+};
+console.log(nthUglyNumber(10));
